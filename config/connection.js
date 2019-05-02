2ee60c335c703b3f3a12450ea488d13d306a5482
ronseide <ron.seide@gmail.com>
2019-05-02 18:44:59 -0400

    In Class Part 2

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Dob120963",
-  database: "burgers_db"
+  user: "nyga2qqyuqfa3k28",
+  password: "vcyw4vcenjdsxe5p",
+  database: "rcohzbzoipojbaws"
 });
 connection.connect(function (err) {
   if (err) {
